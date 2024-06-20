b54fc1b3ea733e8816fd62f894015459f82c6a85
matytyma <matytyma22@gmail.com>
2024-06-20 21:02:16 +0200

    Only allow admins to execute reload command by default

--- src/main/kotlin/dev/matytyma/command/admin/ReloadCommand.kt
+++ src/main/kotlin/dev/matytyma/command/admin/ReloadCommand.kt
@@ -4,12 +4,17 @@ import dev.kord.common.entity.PresenceStatus
 import dev.kord.core.behavior.interaction.respondEphemeral
 import dev.kord.core.behavior.interaction.response.edit
 import dev.kord.core.entity.interaction.ChatInputCommandInteraction
+import dev.kord.rest.builder.interaction.ChatInputCreateBuilder
 import dev.matytyma.*
 import dev.matytyma.command.ChatInputCommand
 import kotlin.time.measureTime
 
 object ReloadCommand : ChatInputCommand {
     override val description = "Reloads all bot commands"
+
+    override suspend fun register(): ChatInputCreateBuilder.() -> Unit = {
+        disableCommandInGuilds()
+    }
 
     override suspend fun onUse(interaction: ChatInputCommandInteraction) {
         val response = interaction.respondEphemeral { content = "Reloading commands..." }
