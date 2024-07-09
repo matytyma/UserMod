f010f184f6017c32cdff250730c97af0e2c86bdd
matytyma <matytyma22@gmail.com>
2024-07-09 19:41:56 +0200

    Store data for reports

---
+++ src/main/kotlin/dev/matytyma/Report.kt
@@ -0,0 +1,17 @@
+package dev.matytyma
+
+import dev.kord.core.entity.Message
+import dev.kord.core.entity.User
+import dev.matytyma.service.ReportService.pendingReports
+import java.time.LocalDateTime
+
+data class Report(
+    val message: Message,
+    val reporter: User,
+) {
+    val confirms = mutableListOf<User>()
+    val rejects = mutableListOf<User>()
+    val timestamp = LocalDateTime.now()
+}
+
+val Message.report get() = pendingReports.first { it.message == this }
--- src/main/kotlin/dev/matytyma/service/ReportService.kt
+++ src/main/kotlin/dev/matytyma/service/ReportService.kt
@@ -2,7 +2,9 @@ package dev.matytyma.service
 
 import dev.kord.core.entity.Message
 import dev.kord.core.entity.User
+import dev.matytyma.Report
 
 object ReportService {
     val pendingReportedMessages = mutableMapOf<User, Message>()
+    val pendingReports = setOf<Report>()
 }
