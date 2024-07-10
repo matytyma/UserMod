a879f060cb66281edfbd6fec01e2f54b0e1a885f
matytyma <matytyma22@gmail.com>
2024-07-10 14:05:48 +0200

    Changes to `ReportModal`
    - Notify selected users about a new report
    - Add link to the original message in both url and a field

--- src/main/kotlin/dev/matytyma/modal/ReportModal.kt
+++ src/main/kotlin/dev/matytyma/modal/ReportModal.kt
@@ -8,22 +8,32 @@ import dev.kord.core.entity.interaction.ModalSubmitInteraction
 import dev.kord.rest.builder.message.actionRow
 import dev.kord.rest.builder.message.embed
 import dev.matytyma.Report
+import dev.matytyma.dotenv
+import dev.matytyma.service.ReportService.REPORT_SCORE_THRESHOLD
 import dev.matytyma.service.ReportService.pendingReports
 import dev.matytyma.service.ReportService.pendingReportedMessages as reportedMessages
 
 object ReportModal : ModalExecutor {
+    private val REPORT_ROLE_MENTION = "<@&${dotenv["REPORT_ROLE_ID"]}>"
+
     override suspend fun onSubmit(interaction: ModalSubmitInteraction) {
         val deferredResponse = interaction.deferEphemeralResponse()
         val message = reportedMessages.remove(interaction.user) ?: return
         val author = message.author ?: return
         val user = interaction.user
 
+        interaction.channel.createMessage(REPORT_ROLE_MENTION).delete()
         interaction.channel.createMessage {
             embed {
                 title = "Message Report"
                 description = "The following message has been reported for breaking the rules"
                 field("Reported Message") { message.content }
                 field("Reason", true) { interaction.textInputs["reason"]!!.value!! }
+                field("Report Score", true) { "0/$REPORT_SCORE_THRESHOLD" }
+                "https://discord.com/channels/${message.getGuild().id}/${message.channelId}/${message.id}".let {
+                    field(it, true)
+                    url = it
+                }
                 author {
                     name = author.effectiveName
                     icon = author.avatar?.cdnUrl?.toUrl()
